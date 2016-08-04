18418cfdf0a8e85a3aebea28ce50ac5d448a849c
GabLoyer <loyer.gab@gmail.com>
2016-08-04 14:14:21 -0400

    Implémentation des montants / Totaux

--- MoneySplitter/MainActivity.cs
+++ MoneySplitter/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
@@ -11,7 +12,17 @@ namespace MoneySplitter
     [Activity(Label = "MoneySplitter", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        List<EditText> listTextMontants;
+        List<EditText> listTextTotaux;
+        List<Button> listBtnAdd;
+        List<CheckBox> listChkTax;
+
+        Button btnCalculate;
+
+        List<float> montants;
+
+        float TAX_TPS = 5;
+        float TAX_TVQ = 9.975f; 
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -22,9 +33,112 @@ namespace MoneySplitter
 
             // Get our button from the layout resource,
             // and attach an event to it
-         /*   Button button = FindViewById<Button>(Resource.Id.MyButton);
+            /*   Button button = FindViewById<Button>(Resource.Id.MyButton);
 
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };*/
+
+            EditText textMontant1;
+            EditText textMontant2;
+            EditText textTotal1;
+            EditText textTotal2;
+            Button btnAdd1;
+            Button btnAdd2;
+            CheckBox chkTax1;
+            CheckBox chkTax2;
+
+            //Initialize variables
+            textMontant1 = FindViewById<EditText>(Resource.Id.textMontant1);
+            textMontant2 = FindViewById<EditText>(Resource.Id.textMontant2);
+            textTotal1 = FindViewById<EditText>(Resource.Id.textTotal1);
+            textTotal2 = FindViewById<EditText>(Resource.Id.textTotal2);
+
+            
+
+            //Initialize buttons
+            btnAdd1 = FindViewById<Button>(Resource.Id.btnAdd1);
+            btnAdd2 = FindViewById<Button>(Resource.Id.btnAdd2);
+
+            btnCalculate = FindViewById<Button>(Resource.Id.btnCalculate);
+
+            btnAdd1.Enabled = false;
+            btnAdd2.Enabled = false;
+
+
+            //Initialize CheckBox
+            chkTax1 = FindViewById<CheckBox>(Resource.Id.chkTax1);
+            chkTax2 = FindViewById<CheckBox>(Resource.Id.chkTax2);
+
+            //Initialize all lists
+            listBtnAdd = new List<Button>();
+            listBtnAdd.Add(btnAdd1);
+            listBtnAdd.Add(btnAdd2);
+
+            listTextMontants = new List<EditText>();
+            listTextMontants.Add(textMontant1);
+            listTextMontants.Add(textMontant2);
+
+            listTextTotaux = new List<EditText>();
+            listTextTotaux.Add(textTotal1);
+            listTextTotaux.Add(textTotal2);
+
+            montants = new List<float>();
+            montants.Add(0);
+            montants.Add(0);
+
+            listChkTax = new List<CheckBox>();
+            listChkTax.Add(chkTax1);
+            listChkTax.Add(chkTax2);
+
+            foreach (EditText text in listTextMontants)
+            {
+                text.TextChanged += TextMontant_TextChanged;
+            }
+
+            foreach (Button btnAdd in listBtnAdd)
+            {
+                btnAdd.Click += BtnAdd_Click;
+            }
+
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            Button btnAdd = (Button)sender;
+            int index = listBtnAdd.IndexOf(btnAdd);
+
+            float montant = montants[index];
+            
+            if (!float.IsNaN(montant))
+            {
+                if (listChkTax[index].Checked)
+                {
+                    montant = (montant * (1 + TAX_TPS / 100.0f)) * (1 + TAX_TVQ / 100.0f);
+                }
+                string total = listTextTotaux[index].Text;
+                if (!String.IsNullOrWhiteSpace(listTextTotaux[index].Text))
+                {
+                    listTextTotaux[index].Text = (float.Parse(total) + montant).ToString();
+                }
+                else
+                {
+                    listTextTotaux[index].Text = montant.ToString();
+                }
+            }
+
+            //Clear TextMontants
+            listTextMontants[index].Text = "";
+            listTextMontants[index].Enabled = false;
+        }
+
+        private void TextMontant_TextChanged(object sender, Android.Text.TextChangedEventArgs e)
+        {
+            EditText textMontant = (EditText)sender;
+            if (!String.IsNullOrWhiteSpace(textMontant.Text))
+            {
+                int index = listTextMontants.IndexOf(textMontant);
+                montants[index] = float.Parse(textMontant.Text);
+                listBtnAdd[index].Enabled = true;       
+            }
         }
     }
 }
--- MoneySplitter/Resources/Resource.Designer.cs
+++ MoneySplitter/Resources/Resource.Designer.cs
@@ -64,37 +64,31 @@ namespace MoneySplitter
 		{
 			
 			// aapt resource value: 0x7f050007
-			public const int button2 = 2131034119;
+			public const int btnAdd1 = 2131034119;
 			
-			// aapt resource value: 0x7f050010
-			public const int button3 = 2131034128;
+			// aapt resource value: 0x7f05000f
+			public const int btnAdd2 = 2131034127;
 			
-			// aapt resource value: 0x7f05000e
-			public const int button4 = 2131034126;
+			// aapt resource value: 0x7f050014
+			public const int btnCalculate = 2131034132;
+			
+			// aapt resource value: 0x7f050012
+			public const int btnPeople = 2131034130;
 			
 			// aapt resource value: 0x7f050006
-			public const int checkBox1 = 2131034118;
+			public const int chkTax1 = 2131034118;
 			
-			// aapt resource value: 0x7f05000c
-			public const int checkBox2 = 2131034124;
+			// aapt resource value: 0x7f05000e
+			public const int chkTax2 = 2131034126;
 			
 			// aapt resource value: 0x7f050008
 			public const int editText1 = 2131034120;
 			
-			// aapt resource value: 0x7f050003
-			public const int editText4 = 2131034115;
-			
-			// aapt resource value: 0x7f050005
-			public const int editText5 = 2131034117;
-			
-			// aapt resource value: 0x7f050009
-			public const int editText6 = 2131034121;
-			
 			// aapt resource value: 0x7f050002
 			public const int imageView1 = 2131034114;
 			
-			// aapt resource value: 0x7f05000f
-			public const int linearLayout1 = 2131034127;
+			// aapt resource value: 0x7f050013
+			public const int linearLayout1 = 2131034131;
 			
 			// aapt resource value: 0x7f050000
 			public const int linearLayout2 = 2131034112;
@@ -102,8 +96,8 @@ namespace MoneySplitter
 			// aapt resource value: 0x7f050004
 			public const int linearLayout3 = 2131034116;
 			
-			// aapt resource value: 0x7f05000b
-			public const int linearLayout4 = 2131034123;
+			// aapt resource value: 0x7f05000c
+			public const int linearLayout4 = 2131034124;
 			
 			// aapt resource value: 0x7f05000a
 			public const int linearLayout5 = 2131034122;
@@ -111,8 +105,26 @@ namespace MoneySplitter
 			// aapt resource value: 0x7f050001
 			public const int linearLayout6 = 2131034113;
 			
+			// aapt resource value: 0x7f050011
+			public const int linearLayout7 = 2131034129;
+			
+			// aapt resource value: 0x7f050005
+			public const int textMontant1 = 2131034117;
+			
 			// aapt resource value: 0x7f05000d
-			public const int linearLayout7 = 2131034125;
+			public const int textMontant2 = 2131034125;
+			
+			// aapt resource value: 0x7f050003
+			public const int textName1 = 2131034115;
+			
+			// aapt resource value: 0x7f05000b
+			public const int textName2 = 2131034123;
+			
+			// aapt resource value: 0x7f050009
+			public const int textTotal1 = 2131034121;
+			
+			// aapt resource value: 0x7f050010
+			public const int textTotal2 = 2131034128;
 			
 			static Id()
 			{
